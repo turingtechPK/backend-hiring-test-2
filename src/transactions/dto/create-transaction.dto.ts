12d03155006c6824fc6d6b71bc43235885919628
Shehriyar Qureshi <thatdevsherry@gmail.com>
2021-12-17 14:50:47 +0500

    chore(transaction): validation in transaction dto

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty } from 'class-validator';
+import { Account } from 'src/accounts/entities/account.entity';
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  sourceAccount: Account;
+
+  @IsNotEmpty()
+  destinationAccount: Account;
+
+  @IsNotEmpty()
+  amount: number;
+}

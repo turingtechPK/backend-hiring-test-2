2e057d67e4e3e51b472ee0f89d2415c6be0e1f1e
Shehriyar Qureshi <thatdevsherry@gmail.com>
2021-12-17 10:44:59 +0500

    chore(): add initial config for sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CustomersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

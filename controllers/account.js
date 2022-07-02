110bb77cdf9336ead591912b4f6ba10f0d851baa
muh-awais <awais8111998@gmail.com>
2022-07-03 04:16:33 +0500

    Fixed some routes and implemented deposit, withdraw and transfered functionality.

--- controllers/account.js
+++ controllers/account.js
@@ -14,7 +14,9 @@ exports.getAllAccounts = catchAsync(async (req, res, next) => {
 });
 
 exports.getAccount = catchAsync(async (req, res, next) => {
-  const account = await Account.findOne({ account: req.params.account });
+  const account = await Account.findOne({
+    accountNumber: req.params.accountNumber,
+  });
   if (!account) {
     return next(new RichError('No account found with that ID', 404));
   }
@@ -36,24 +38,83 @@ exports.createAccount = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateAccount = catchAsync(async (req, res, next) => {
-  const account = await Account.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
+exports.depositAmount = catchAsync(async (req, res, next) => {
+  const account = await Account.findOne({
+    accountNumber: req.body.accountNumber,
   });
   if (!account) {
     return next(new RichError('No account found with that ID', 404));
   }
+  const saldo = parseInt(account.amount, 10);
+  const valueAfterDeposit = saldo + req.body.amount;
+  await Account.updateOne(
+    { accountNumber: req.body.accountNumber },
+    { amount: valueAfterDeposit }
+  );
   res.status(200).json({
     status: 'success',
-    data: {
-      account,
-    },
+    message: 'Deposit executed successfully.',
+  });
+});
+
+exports.withdrawAmount = catchAsync(async (req, res, next) => {
+  const account = await Account.findOne({
+    accountNumber: req.body.accountNumber,
+  });
+  if (!account) {
+    return next(new RichError('No account found with that ID', 404));
+  }
+  const withdrawValue = parseInt(account.amount, 10);
+  const valueAfterWithdraw = withdrawValue - req.body.amount;
+  await Account.updateOne(
+    { accountNumber: req.body.accountNumber },
+    { amount: valueAfterWithdraw }
+  );
+  res.status(200).json({
+    status: 'success',
+    message: 'Withdraw executed successfully.',
+  });
+});
+
+exports.transferAmount = catchAsync(async (req, res, next) => {
+  const amountOfTransaction = req.body.amount;
+  const accountTransferOut = await Account.findOne({
+    accountNumber: req.body.outAccount,
+  });
+  if (!accountTransferOut) {
+    return next(new RichError('No account found with that ID', 404));
+  }
+  if (accountTransferOut.amount >= amountOfTransaction) {
+    const transferWithdraw = accountTransferOut.amount - amountOfTransaction;
+    await Account.findByIdAndUpdate(accountTransferOut._id, {
+      amount: transferWithdraw,
+    });
+  } else {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'There is no balance available for the transaction to complete.',
+    });
+  }
+  const accountTransferIn = await Account.findOne({
+    accountNumber: req.body.inAccount,
+  });
+  if (!accountTransferIn) {
+    return next(new RichError('No account found with that ID', 404));
+  }
+  const transferDeposit = accountTransferIn.amount + amountOfTransaction;
+  await Account.findByIdAndUpdate(accountTransferIn._id, {
+    amount: transferDeposit,
+  });
+  res.status(200).json({
+    status: 'success',
+    message: 'Transfer executed successfully.',
   });
 });
 
 exports.deleteAccount = catchAsync(async (req, res, next) => {
-  const account = await Account.findOneAndDelete(req.body.account);
+  const account = await Account.findOneAndDelete({
+    accountNumber: req.body.accountNumber,
+  });
   if (!account) {
     return next(new RichError('No account found with that ID', 404));
   }
--- routes/api/account.js
+++ routes/api/account.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get('/', accountController.getAllAccounts);
 router.get('/:account', accountController.getAccount);
 router.post('/create', accountController.createAccount);
-router.post('/deposit', accountController.updateAccount);
-router.post('/withdraw', accountController.updateAccount);
-router.put('/transfer', accountController.updateAccount);
+router.post('/deposit', accountController.depositAmount);
+router.post('/withdraw', accountController.withdrawAmount);
+router.put('/transfer', accountController.transferAmount);
 router.delete('/delete', accountController.deleteAccount);
 
 module.exports = router;

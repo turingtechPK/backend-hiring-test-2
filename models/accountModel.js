5289106e6bae58541e026af74199e5ad59417a08
muh-awais <awais8111998@gmail.com>
2022-07-03 06:45:29 +0500

    Generated random bank account number and encrypted a password.

--- models/accountModel.js
+++ models/accountModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
-const AccountSchema = new Schema({
+const accountSchema = new Schema({
   ownerName: {
     type: String,
     required: true,
@@ -18,15 +19,19 @@ const AccountSchema = new Schema({
   },
   accountNumber: {
     type: Number,
-    required: true,
     unique: true,
   },
   amount: {
     type: Number,
-    default: 0,
-    required: true,
+    default: 1,
     trim: true,
   },
 });
 
-module.exports = mongoose.model('Account', AccountSchema);
+accountSchema.pre('save', async function (next) {
+  this.accountNumber = parseInt(Math.random().toString().substr(2, 6), 10);
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model('Account', accountSchema);

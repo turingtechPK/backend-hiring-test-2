a7e490d13239f69c0cde769284cba9a6239dbfdc
muh-awais <awais8111998@gmail.com>
2022-07-03 07:50:04 +0500

    Implemented verify account functionality using jwt.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const RichError = require('./utils/richError');
 const accountRouter = require('./routes/api/account');
+const authRouter = require('./routes/api/auth');
 
 const app = express();
 
@@ -12,7 +13,8 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.use('/api/v1/accounts', accountRouter);
+app.use('/api/v1/account', accountRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.all('*', (req, res, next) => {
   next(new RichError(`Can't find ${req.originalUrl} on this server!`, 404));
---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const Account = require('../models/accountModel');
+
+module.exports.verifyAccount = async (req, res) => {
+  if (!req.body && !req.body.email && !req.body.password) {
+    res.status(204).json({
+      status: 'fail',
+      message: 'Please provide a valid email or password!',
+    });
+  }
+  const account = await Account.find({ email: `${req.body.email}` });
+  const doMatch = await bcrypt.compare(req.body.password, account[0].password);
+  if (doMatch) {
+    const token = jwt.sign({ id: account[0]._id }, process.env.SECRET_HASH, {
+      expiresIn: process.env.EXPIRES_IN,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        _id: account[0]._id,
+        ownerName: account[0].ownerName,
+        email: account[0].email,
+        accountNumber: account[0].accountNumber,
+        amount: account[0].amount,
+        token,
+      },
+    });
+  } else {
+    return res.status(403).json({
+      status: 'fail',
+      message: 'Incorrect password!',
+    });
+  }
+};

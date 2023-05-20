465603552819542e2ccaef01984751cb7d4b9c96
waleedkayani <waleedkayani14@yahoo.com>
2023-05-20 21:12:10 +0500

    added search by author and search by publish date

--- src/routes/volume.js
+++ src/routes/volume.js
@@ -5,9 +5,7 @@ const Volume = require("../models/Volume");
 const { getUserAccountType } = require("../services/userServices");
 
 router.get("/ping", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Connected to Authentication API", req: req.body });
+  res.status(200).json({ message: "Connected to Volume API", req: req.body });
 });
 
 router.post("/", async (req, res) => {
@@ -44,7 +42,29 @@ router.get("/", async (req, res) => {
     const result = await Volume.find({ name: name });
     res.status(200).json({ message: "Successful", result });
   } catch {
-    res.status(500).json({ message: "Failed to locate Bookshelf" });
+    res.status(500).json({ message: "Failed to locate volume" });
+  }
+});
+
+router.get("/publish_date", async (req, res) => {
+  const { published_date } = req.body;
+
+  try {
+    const result = await Volume.find({ published_date: published_date });
+    res.status(200).json({ message: "Successful", result });
+  } catch {
+    res.status(500).json({ message: "Failed to locate Volume" });
+  }
+});
+
+router.get("/author", async (req, res) => {
+  const { author } = req.body;
+
+  try {
+    const result = await Volume.find({ author: author });
+    res.status(200).json({ message: "Successful", result });
+  } catch {
+    res.status(500).json({ message: "Failed to locate Volume" });
   }
 });
 

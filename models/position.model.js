ed98eef83aa91f7c1351479aa12182c0ce48b3df
Shayan Amir <i190749@nu.edu.pk>
2023-05-31 15:37:43 +0500

    updated position model

--- models/position.model.js
+++ models/position.model.js
@@ -2,24 +2,20 @@ const mongoose = require('mongoose');
 
 const positionSchema = new mongoose.Schema({
     volume: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Volume',
-      required: true
-    },
-    rating: {
-      type: Number,
-      required: true
-    },
-    text: {
-      type: String,
-      required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Volume',
+        required: true
     },
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-  });
+    position: {
+        type: Number,
+        required: true
+    },
+});
 
 const Position = mongoose.model('Position', positionSchema);
 Position.createIndexes();

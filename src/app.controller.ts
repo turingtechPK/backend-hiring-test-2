be922ca07d5a6aeda5f67abd5356cab70ef434ad
Usama Khatab <i190493@nu.edu.pk>
2023-06-17 22:46:12 +0500

    - final commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,21 @@ import { AppService } from './app.service';
 from the AppService. */
 @Controller()
 export class AppController {
+  /**
+   * This is a constructor function that takes an instance of the AppService class as a parameter and
+   * assigns it to a private readonly property.
+   * @param {AppService} appService - The `appService` parameter is a dependency injection of the
+   * `AppService` class. It allows the class that contains this constructor to use the methods and
+   * properties of the `AppService` class. The `private readonly` keywords indicate that the
+   * `appService` parameter is a private property of the
+   */
   constructor(private readonly appService: AppService) {}
 
+  /* `@Get()` is a decorator that specifies that the following method should handle HTTP GET requests.
+  `getHello()` is a method that returns a string by calling the `getHello()` method of the
+  `AppService` class, which is injected into the `AppController` class through its constructor. When
+  a GET request is made to the root URL of the application, this method is called and the string
+  returned by `getHello()` is sent as the response. */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { UserService } from '../src/User/User.service';
+import { BookshelfService } from '../src/BookShelf/Bookshelf.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let service: UserService;
+  let bookshelfService: BookshelfService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      //providers: [UserService],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    service = moduleFixture.get<UserService>(UserService);
+    bookshelfService = moduleFixture.get<BookshelfService>(BookshelfService);
+    // app = moduleFixture.createNestApplication();
+    // await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
+  it('Login User', async() => {
+    const actualLogin = await service.login("abdusman","myPass");
+    expect(actualLogin).toBe(null);
   });
 });
